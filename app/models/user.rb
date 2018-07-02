4ce14cf0667afd7bb1a4e19412fff04d73873b76
Luis Ballinas <lu15bytes@gmail.com>
2018-07-02 19:08:50 +0000

    ADDED: length validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
      attr_accessor :name, :email
      has_many :microposts
-     validates :name, presence: true
-     validates :email, presence: true
 
+     validates :name,  presence: true, length: { maximum: 50 }
+     validates :email, presence: true, length: { maximum: 255 }
 end
