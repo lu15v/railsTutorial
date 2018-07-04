10000f2cf957bb499212e0d93808423e21bba7f0
Luis Ballinas <lu15bytes@gmail.com>
2018-07-04 20:29:21 +0000

    FIXED: sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
+  
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
@@ -71,6 +71,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 end
